71c1030f47b5b4102830aef67d98a9ed7d42b385
d4park@ucsd.edu <d4park@ucsd.edu@98471b84-2354-11df-b9ed-85270ab41615>
2010-10-11 02:35:47 +0000

    added comments for documenation in p3dfft.h

--- trunk/include/p3dfft.h
+++ trunk/include/p3dfft.h
@@ -38,11 +38,16 @@
 #define FORTNAME(NAME) NAME##_
 
 #elif defined GCC
+
+/* get GCC version info */
 #define GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
+
+/* check for GCC 4.1.2 */
 #if GCC_VERSION <= 40102 || GCC_VERSION < 40404
 #define FORT_MOD_NAME(NAME) __p3dfft__##NAME
 #define FORTNAME(NAME) NAME##_
 
+/* check for GCC 4.4.4 */
 #elif GCC_VERSION >= 40404
 #define FORT_MOD_NAME(NAME) __p3dfft_MOD_##NAME
 #define FORTNAME(NAME) NAME##_
